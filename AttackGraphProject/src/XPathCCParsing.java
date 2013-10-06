6bc85801ae6397469318eaec8f82e799b3cbf34e
Abhilash Karanam <akarana1@asu.edu>
2013-10-05 23:35:29 -0700

    neethu changes

--- AttackGraphProject/src/XPathCCParsing.java
+++ AttackGraphProject/src/XPathCCParsing.java
@@ -1,36 +1,69 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import org.w3c.dom.*;
+import java.io.PrintWriter;
+ 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+ 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import javax.xml.parsers.*;
-import javax.xml.xpath.*;
-
+ 
 public class XPathCCParsing {
-
-  public static void main(String[] args) 
-   throws ParserConfigurationException, SAXException, 
-          IOException, XPathExpressionException {
-	  
-    DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-    domFactory.setNamespaceAware(true); // never forget this!
-    DocumentBuilder builder = domFactory.newDocumentBuilder();
-    try{
-    Document doc = builder.parse("C:\\Users\\Neethu\\Desktop\\CC\\books.xml");
-    //Document doc = builder.parse("C:\\Users\\Neethu\\Desktop\\CC\\DependencyCheckReport.xml");
-
-    XPathFactory factory = XPathFactory.newInstance();
-    XPath xpath = factory.newXPath();
-    XPathExpression expr 
-    // = xpath.compile("//vulnerabilities/vulnerability/name/text()");
-    = xpath.compile("//book/text()");
-
-    Object result = expr.evaluate(doc, XPathConstants.NODESET);
-    NodeList nodes = (NodeList) result;
-    for (int i = 0; i < nodes.getLength(); i++) {
-        System.out.println(nodes.item(i).getNodeValue()); 
+    public static void main(String[] args) {
+ 
+        try {
+        	//Write results into outpust file
+        	PrintWriter writer = new PrintWriter("C:\\Users\\Neethu\\Desktop\\CC\\parseOutputFile.txt", "UTF-8");
+				
+			//Parse XML File        	
+            FileInputStream file = new FileInputStream(new File("C:\\Users\\Neethu\\Desktop\\CC\\DependencyCheckReport.xml"));
+                 
+            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+             
+            DocumentBuilder builder =  builderFactory.newDocumentBuilder();
+             
+            Document xmlDocument = builder.parse(file);
+ 
+            XPath xPath =  XPathFactory.newInstance().newXPath();
+ 
+            System.out.println("*************************");
+            //String expression1 = "//dependencies/dependency/fileName";
+            //String expression = "//dependency/vulnerabilities/vulnerability/*";
+            String expression = "//dependencies/dependency/vulnerabilities/vulnerability/*";
+            //System.out.println(expression);
+            NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                //System.out.println(nodeList.item(i).getFirstChild().getNodeValue());
+            	System.out.println("Write into output file");
+                //bw.write(nodeList.item(i).getFirstChild().getNodeValue());
+            	writer.println(nodeList.item(i).getFirstChild().getNodeValue());    		
+            	
+            	
+            }
+            writer.close();
+            System.out.println("*************************");
+ 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }       
     }
-  }catch(Exception e)
-  {
-	  e.printStackTrace();
-  }
-  }
+
 }
